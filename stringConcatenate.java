a1ed846502eb3cad0f871e6e06ff0f289f1860d7
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-23 17:56:54 +0530

    Create stringConcatenate.java

---
+++ stringConcatenate.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	String s=in.next();
+    String a=in.next();
+		System.out.println(s.concat(a));
+ in.close();
+ }
+}
