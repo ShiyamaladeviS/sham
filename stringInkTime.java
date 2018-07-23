9e4c8c5e17fc330ff43caa392d4c9633b15f1d3c
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-23 17:32:55 +0530

    Create stringInkTime.java

---
+++ stringInkTime.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	String s=in.next();
+			 int k=in.nextInt();
+			for(int i=1;i<=k;i++)
+			{			
+				System.out.println(s);
+			}
+			System.out.println();
+ in.close();
+ }
+}
