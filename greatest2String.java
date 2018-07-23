7cec512d080196480276586a2789f2b9885c4d95
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-23 17:53:37 +0530

    Create greatest2String.java

---
+++ greatest2String.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	String s=in.next();
+    String a=in.next();
+		if((s.length())>(a.length()))	{
+			System.out.println(s);
+		}else{
+			System.out.println(a);
+		}
+ in.close();
+ }
+}
