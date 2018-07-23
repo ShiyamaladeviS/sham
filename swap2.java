6424ca44cca8069345f58cec94d80852d41ec435
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-23 17:20:38 +0530

    Update swap2.java

--- swap2.java
+++ swap2.java
@@ -4,9 +4,10 @@ public static void main(String[] args){
 	Scanner in=new Scanner(System.in);
 			 int a=in.nextInt();
 			 int b=in.nextInt();
-			 a=a^b;
-			 b=b^a;
-			 a=a^b;
+	                  int temp=0;
+			 temp=a;;
+			 a=b;
+			 b=temp;
 			 System.out.println(a+" "+b);
  in.close();
  }
