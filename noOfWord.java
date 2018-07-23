d885da1e464db3c066be34c03c9f54c974ad55c6
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-23 11:58:06 +0530

    Create noOfWord.java

---
+++ noOfWord.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+			 String str=in.nextLine();
+			String[] s=str.split(" ");
+System.out.println(s.length);
+ in.close();
+ }
+}
