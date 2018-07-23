4832d905079a2b858b4b225e4a5fb1143889537f
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-23 18:01:52 +0530

    Create Nrange10.java

---
+++ Nrange10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int s=in.nextInt();
+  if(s>=0 && s<=10){
+		System.out.println("yes");
+  }else{
+	  System.out.println("no");
+  }
+ in.close();
+ }
+}
