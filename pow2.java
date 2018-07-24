c17395763e27f2bcf031161113d30c715b1176b4
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-24 14:09:36 +0530

    Create pow2.java

---
+++ pow2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	if(n%2==0){
+	System.out.print("yes");
+	}else{
+		System.out.print("No");
+	}
+ in.close();
+ }
+}
