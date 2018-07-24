8633b889b6a24ae0f0a8b077a594100247d91aad
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-24 17:55:27 +0530

    Create naturalNo.java

---
+++ naturalNo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int sum=0;
+	for(int i=1;i<=a;i++){
+		sum+=i;
+	}
+System.out.println(sum);
+
+	in.close();
+ }
+}
