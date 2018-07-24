a924f8ecf6a39360c4a8c049c978ed39c091f66a
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-24 12:19:39 +0530

    Rename nextNo.java to nextNumber.java

---
+++ nextNumber.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int s=in.nextInt();
+  System.out.println(s+1);
+ in.close();
+ }
+}
