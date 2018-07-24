27681fb21333f6407eb145e1cf4922d17e0dc60f
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-24 17:47:35 +0530

    Create checkRepetiton.java

---
+++ checkRepetiton.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int b=in.nextInt();
+	int c[]=new int[a];
+	int count=0;
+for(int i=0;i<a;i++){
+	c[i]=in.nextInt();
+}
+for(int i=0;i<a;i++){
+	if(c[i]==b){
+		count++;
+	}
+}
+if(count>=1){
+System.out.println("yes");
+}else{
+	System.out.println("no");
+}
+	in.close();
+ }
+}
