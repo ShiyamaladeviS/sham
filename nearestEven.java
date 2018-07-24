4576bc34f3502b10d70c8d32c39c677160010873
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-24 17:03:45 +0530

    Create nearestEven.java

---
+++ nearestEven.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+
+	if(a%2==0){
+		System.out.println(a);
+		}else{
+			System.out.println(a-1);
+		}
+	in.close();
+ }
+}
