281d864af207a77686ba0af4894c19343d3d6a27
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-24 14:03:38 +0530

    Create sum2.java

---
+++ sum2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int a=in.nextInt();
+	System.out.print(n+a);
+ in.close();
+ }
+}
