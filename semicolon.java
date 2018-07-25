2c2ab64be55f5033b3ce400cf874c8d446109047
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-25 10:27:00 +0530

    Create semicolon.java

---
+++ semicolon.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class guvi {
+	public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+     String s=in.next();
+ if(System.out.printf(s) == null)
+     in.close();
+	}
+}
