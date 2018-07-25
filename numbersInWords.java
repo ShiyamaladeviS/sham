e300eee697166373e85db09de80de72e094e0cfb
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-25 09:45:57 +0530

    Create numbersInWords.java

---
+++ numbersInWords.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class guvi {
+	public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+      int n=in.nextInt();
+      String[] s={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+     System.out.println(s[n-1]);
+   in.close();
+	}
+}
