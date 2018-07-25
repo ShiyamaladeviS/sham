d5127e7804fc2828dc2812d79b86e35b9b81a367
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-25 17:18:02 +0530

    Create mul10.java

---
+++ mul10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class guvi {
+	public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+     int n=in.nextInt();
+     int b=n%10;
+    	int c=10-b;
+    		 System.out.println(c+n);
+    in.close();
+	}
+}
