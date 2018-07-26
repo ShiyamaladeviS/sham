e7d49cc6e94ca8ca48be593a04af5a35554647bf
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-26 18:42:47 +0530

    Create mul13.java

---
+++ mul13.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class guvi {
+	public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+int s=in.nextInt();
+ if(s%13==0){
+	System.out.println("yes");
+ }
+ else{
+	 System.out.println("no");
+ }
+      in.close();
+	}
+}
