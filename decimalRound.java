f93a3aa40856d2cf1aa76255fd56ef512760eb66
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-26 18:16:50 +0530

    Create decimalRound.java

---
+++ decimalRound.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class guvi {
+	public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+ float n=in.nextFloat();
+ int a=(int) Math.ceil(n);
+ System.out.println(a); 
+      in.close();
+	}
+}
